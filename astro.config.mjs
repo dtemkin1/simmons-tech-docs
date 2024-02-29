1a7f15f74459b4d85ec5d172f8e7d0549acb78a4
Diego Temkin <65834932+dtemkin1@users.noreply.github.com>
2024-02-29 14:26:29 -0500

    add simmons hall styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,35 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Simmons Tech",
+      logo: {
+        src: "./src/assets/simmons-logo.png",
+      },
+      social: {
+        github: "https://github.com/simmons-tech",
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/dtemkin1/simmons-tech-docs/edit/main/docs/",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+  ],
 });
